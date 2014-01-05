fad4a8005cc92e5a61a2222095099b64e5b0561f
geht <marwinrieger@googlemail.com>
2014-01-05 17:56:47 +0100

    added attributes from the class diagramm to the EditorWindow(Controller), as well as comments

--- Editor/EditorWindow.cs
+++ Editor/EditorWindow.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////
+// file:	editorwindow.cs
+//
+// summary:	Implements the editorwindow class
+// This is the main window of the entire program, as well as the main entrance point.
+// The Editor Window controlls the main tasks and delegates complex task to the other dedicated controllers.
+// It also controls the communication between the model and the view, meaning it builds the view and saves/deletes/changes data in the model.
+// 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,27 +20,156 @@ using System.Windows.Forms;
 
 namespace ARdevKit
 {
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// <summary>
+    /// Form for viewing the editor. This is the main form of the program.
+    /// </summary>
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
     public partial class EditorWindow : Form
     {
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// if true the debug window will be opened when starting the test mode on the device.
+        /// </summary>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private bool startDebugModeDevice;
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// if true the debug window will be opened when starting the test mode locally.
+        /// </summary>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private bool startDebugModeLocal;
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// Categories the element belongs to.
+        /// List of scene element categories. 
+        /// </summary>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //not yet implemented...
+        //private List<SceneElementCategory> elementCategories;
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// Gets or sets a value indicating whether to start debug mode if test mode is started on the
+        /// device.
+        /// </summary>
+        ///
+        /// <value>
+        /// if true the debug window will be opened when starting the test mode on the device.
+        /// </value>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public bool StartDebugModeDevice
+        {
+            get { return startDebugModeDevice; }
+            set { startDebugModeDevice = value; }
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// Gets or sets a value indicating whether to start debug mode locally.
+        /// </summary>
+        ///
+        /// <value>
+        /// if true the debug window will be opened when starting the test mode locally.
+        /// </value>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public bool StartDebugModeLocal
+        {
+            get { return startDebugModeLocal; }
+            set { startDebugModeLocal = value; }
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// Gets or sets the categories the element belongs to.
+        /// </summary>
+        ///
+        /// <value>
+        /// The element categories.
+        /// </value>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //not yet implemented...
+        //private List<SceneElementCategory> ElementCategories
+        //{
+        //    get { return elementCategories; }
+        //    set { elementCategories = value; }
+        //}
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// Default constructor. initializes components on startup.
+        /// </summary>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
         public EditorWindow()
         {
             InitializeComponent();
         }
 
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// Event handler. Called by Editor for load events. actions to do when the editor is loaded.
+        /// </summary>
+        ///
+        /// <param name="sender">   Source of the event. </param>
+        /// <param name="e">        Event information. </param>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
         private void Editor_Load(object sender, EventArgs e)
         {
-
+            //stub
         }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// Event handler. Called by tsm_editor_menu_file_new for click events.
+        /// </summary>
+        ///
+        /// <exception cref="NotImplementedException">  Thrown when the requested operation is
+        ///                                             unimplemented. </exception>
+        ///
+        /// <param name="sender">   Source of the event. </param>
+        /// <param name="e">        Event information. </param>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
 
         private void tsm_editor_menu_file_new_Click(object sender, EventArgs e)
         {
-
+            //stub
+            throw new NotImplementedException();
         }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// Event handler. Called by tsm_editor_menu_file_exit for click events. exits the program.
+        /// </summary>
+        ///
+        /// <param name="sender">   Source of the event. </param>
+        /// <param name="e">        Event information. </param>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
 
         private void tsm_editor_menu_file_exit_Click(object sender, EventArgs e)
         {
             System.Windows.Forms.Application.Exit();
         }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// Event handler. Called by tsm_editor_menu_test_loadImage for click events. Starts the test
+        /// mode for images.
+        /// </summary>
+        ///
+        /// <param name="sender">   Source of the event. </param>
+        /// <param name="e">        Event information. </param>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
 
         private void tsm_editor_menu_test_loadImage_Click(object sender, EventArgs e)
         {
@@ -38,10 +177,37 @@ namespace ARdevKit
             testWindow.Show();
         }
 
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// Event handler. Called by tsm_editor_menu_test_loadVideo for click events. Starts the test
+        /// mode for videos.
+        /// </summary>
+        ///
+        /// <param name="sender">   Source of the event. </param>
+        /// <param name="e">        Event information. </param>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
         private void tsm_editor_menu_test_loadVideo_Click(object sender, EventArgs e)
         {
             TestWindow testWindow = new TestWindow();
             testWindow.Show();
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <summary>
+        /// Event handler. Called by tsm_editor_menu_file_open for click events.
+        /// </summary>
+        ///
+        /// <exception cref="NotImplementedException">  Thrown when the requested operation is
+        ///                                             unimplemented. </exception>
+        ///
+        /// <param name="sender">   Source of the event. </param>
+        /// <param name="e">        Event information. </param>
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void tsm_editor_menu_file_open_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
