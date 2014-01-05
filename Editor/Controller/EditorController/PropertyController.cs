85ce44cf88e0633c3ea7427d53edc46ad802e557
Bareus <sed_chan@hotmail.de>
2014-01-05 18:44:16 +0100

    Added PropertyController.cs and hopefully also all the folders I made....

---
+++ Editor/Controller/EditorController/PropertyController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace Controller.EditorController
+{
+    /// <summary>
+    /// The PropertyController contains static methods to add and edit customUserEvents.
+    /// </summary>
+    class PropertyController
+    {
+        public static void addCustomUserEvent(AbstractAugmentation selectedElement)
+        {
+
+        }
+
+        public static void editCusomUserEvent(File customUserEvent)
+        {
+
+        }
+
+        public static void updatePropertyPanel(IPreviewable selectedElement)
+        {
+
+        }
+    }
+}
