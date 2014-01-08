6da3e12af0c03a432165d7c3622d4171de4a8872
Bareus <sed_chan@hotmail.de>
2014-01-08 16:47:35 +0100

    Added some comments and delete methods in AbstractAugmentation
    Added some comments and delete-methods in PropertyController
    
    updatePropertyPanel is still throwing NotImplementedException, because this method could be unnecessary.

--- Editor/Controller/EditorController/PropertyController.cs
+++ Editor/Controller/EditorController/PropertyController.cs
@@ -14,12 +14,33 @@ namespace Controller.EditorController
     /// </summary>
     class PropertyController
     {
+        /// <summary>
+        /// Adds a customUserEvent to a selected element.
+        /// </summary>
+        /// <param name="selectedElement">Current selected AbstractAugmentation</param>
+        /// <param name="content">Content of the customUserEvent</param>
         public static void addCustomUserEvent(AbstractAugmentation selectedElement, string[] content)
         {
             string name = "customUserEvent" + (selectedElement.listCounter() + 1);
             selectedElement.addCustomUserEvent(name, content);
         }
 
+        /// <summary>
+        /// Deletes a customUserEvent from a selected element.
+        /// </summary>
+        /// <param name="selectedElement">Current selected AbstractAugmentation</param>
+        /// <param name="name">Name/ID of the customUserEvent</param>
+        public static void deleteCustomUserEvent(AbstractAugmentation selectedElement, string name)
+        {
+            selectedElement.deleteCustomUserEvent(name);
+        }
+
+        /// <summary>
+        /// Gets a customUserEvent from a selected element.
+        /// </summary>
+        /// <param name="selectedElement">Current selected element</param>
+        /// <param name="name">Name/ID of the customUserEvent</param>
+        /// <returns>Returns the content of the customUserEvent</returns>
         public static string[] getCusomUserEvent(AbstractAugmentation selectedElement, string name)
         {
             return selectedElement.getCustomUserEvent(name);
@@ -27,7 +48,7 @@ namespace Controller.EditorController
 
         public static void updatePropertyPanel(IPreviewable selectedElement)
         {
-
+            throw new NotImplementedException();
         }
     }
 }
--- Editor/Model/Project/AbstractAugmentation.cs
+++ Editor/Model/Project/AbstractAugmentation.cs
@@ -10,6 +10,10 @@ namespace Model.Project
     /// This class is an abstract Element which every AbstractTrackable has. Other elements like a diagramm
     /// or images must be succeeded by this class.
     /// </summary>
+    /// 
+    /// This class is till TODO!!! Set and Get methods from some variables are still missing, also some methods from the 
+    /// design patern and comments for the documentation.
+    /// 
     public class AbstractAugmentation
     {
         /// <summary>
@@ -27,19 +31,44 @@ namespace Model.Project
         /// </summary>
         private bool isVisible;
 
+        public AbstractAugmentation()
+        {
+            customUserEvent = new List<CustomUserEvent>();
+            coordinateSystemId = 0;
+            isVisible = false;
+        }
+
+        public AbstractAugmentation(int coordId, bool isVisible)
+        {
+            customUserEvent = new List<CustomUserEvent>();
+            coordinateSystemId = coordId;
+            this.isVisible = isVisible;
+        }
+
         public int listCounter()
         {
             return customUserEvent.Count;
         }
 
         /// <summary>
-        /// Adds a User generated customUserEvent to the List.
+        /// Adds a user-generated customUserEvent to the List.
         /// </summary>
         /// <param name="name">Name of the customUserEvent</param>
         /// <param name="content">The content of the customUserEvent</param>
         public void addCustomUserEvent(string name, string[] content)
         {
             customUserEvent.Add(new CustomUserEvent(name, content));
+        }
+
+        public void deleteCustomUserEvent(string name)
+        {
+            foreach (CustomUserEvent cue in customUserEvent)
+            {
+                if (cue.Name == name)
+                {
+                    customUserEvent.Remove(cue);
+                }
+            }
         }
 
         /// <summary>
