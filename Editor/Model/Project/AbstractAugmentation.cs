f66372d0e3a7d926440183952dda4056004acafd
Lizzard2397 <Lizzard2397@gmail.com>
2014-01-13 19:01:59 +0100

    fix some preparations for merge with PreviewController

--- Editor/Model/Project/AbstractAugmentation.cs
+++ Editor/Model/Project/AbstractAugmentation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ARdevKit.Model.Project;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -62,34 +63,6 @@ namespace Model.Project
         {
             get { return customUserEvent; }
 
-        }
-    }
-
-    /// <summary>
-    /// Just a class for AbstractAugmentations. With this we are able to List all the customUserEvents. 
-    /// See issue #13 for reason of this class.
-    /// </summary>
-    public class CustomUserEvent
-    {
-        private string name;
-        private string[] content;
-
-        public CustomUserEvent(string name, string[] content)
-        {
-            this.name = name;
-            this.content = content;
-        }
-
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-
-        public string[] Content
-        {
-            get { return content; }
-            set { content = value; }
         }
     }
 }
---
+++ Editor/Model/Project/CustumUserEvent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARdevKit.Model.Project
+{
+    /// <summary>
+    /// Just a class for AbstractAugmentations. With this we are able to List all the customUserEvents. 
+    /// See issue #13 for reason of this class.
+    /// </summary>
+    public class CustomUserEvent
+    {
+        private string name;
+        private string[] content;
+
+        public CustomUserEvent(string name, string[] content)
+        {
+            this.name = name;
+            this.content = content;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string[] Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+    }
+}
