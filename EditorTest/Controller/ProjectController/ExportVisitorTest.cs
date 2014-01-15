b1336a47fd0070abdbc52a2639f91de9dd2116ad
Imanuel Richter <imanuel.richter@gmail.com>
2014-01-15 10:02:42 +0100

    Added a unit test project (EditorTest) which should have the same structure like (Editor)

---
+++ EditorTest/Controller/ProjectController/ExportVisitorTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EditorTest
+{
+    [TestClass]
+    public class ExportVisitorTest
+    {
+        [TestMethod]
+        public void VisitProjectTest()
+        {
+        }
+    }
+}
