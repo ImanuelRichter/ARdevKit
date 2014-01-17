2a7719512e38b97347bb69a95051517b798fcbfe
Bareus <sed_chan@hotmail.de>
2014-01-17 21:49:01 +0100

    Restructed BarGraph, DbSource and IDMarker (also added tags for the PropertyGrid)

--- Editor/Model/Project/DbSource.cs
+++ Editor/Model/Project/DbSource.cs
@@ -6,31 +6,66 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 
+using System.ComponentModel;
+
 namespace ARdevKit.Model.Project
 {
-    /// <summary>
-    /// 
-    /// </summary>
+    /// <summary>   A database source. </summary>
     [Serializable]
+    [TypeConverterAttribute(typeof(ExpandableObjectConverter))]
     public class DbSource : AbstractSource
     {
+        /// <summary>   The connection. </summary>
         private DbConnection connection;
+        /// <summary>   Gets or sets the connection. </summary>
+        ///
+        /// <value> The connection. </value>
+        [CategoryAttribute("General")]
+        public DbConnection Connection
+        {
+            get { return connection; }
+            set { connection = value; }
+        }
 
+        /// <summary>   Default constructor. </summary>
+        public DbSource()
+        {
+            ; // missing initialization
+        }
+
+        /// <summary>   ToDo Summary is missing. </summary>
+        ///
+        /// <exception cref="NotImplementedException"> Thrown when the requested operation is
+        ///     unimplemented. </exception>
+        ///
+        /// <param name="visitor">  . </param>
         public override void accept(Controller.ProjectController.AbstractProjectVisitor visitor)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>   ToDo Summary is missing. </summary>
+        ///
+        /// <exception cref="NotImplementedException"> Thrown when the requested operation is
+        ///     unimplemented. </exception>
+        ///
+        /// <returns>   The property list. </returns>
         public override List<View.AbstractProperty> getPropertyList()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>   ToDo Summary is missing. </summary>
+        ///
+        /// <returns>   The preview. </returns>
         public override Bitmap getPreview()
         {
             return Properties.Resources.sourcePreview_normal_;
         }
 
+        /// <summary>   ToDo Summary is missing. </summary>
+        ///
+        /// <returns>   The icon. </returns>
         public override Bitmap getIcon()
         {
             return Properties.Resources.sourcePreview_small_;
