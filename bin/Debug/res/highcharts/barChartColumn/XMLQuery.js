80622167f8519e1b5ba8ba311f13abd06e5feb38
Imanuel Richter <imanuel.richter@gmail.com>
2014-01-26 14:17:37 +0100

    Added some debug information and updated some export issues as well

--- bin/Debug/res/highcharts/barChartColumn/XMLQuery.js
+++ bin/Debug/res/highcharts/barChartColumn/XMLQuery.js
@@ -1,25 +1,34 @@
-var $xml = $(xml);
-$xml.find('categories item').each(function(i, category)
+function query(dataPath, id, options)
 {
-	arel.Plugin.Chart1.options.xAxis.categories.push($(category).text());
-});
-$xml.find('series').each(function(i, series)
-{
-	var seriesOptions =
+	$.get(dataPath, function(xml)
 	{
-		name: $(series).find('name').text(),
-		color: {
-			linearGradient: { x1: $(series).find('x1').text(), x2: $(series).find('x2').text(), y1: $(series).find('y1').text(), y1: $(series).find('y2').text() },
-			stops: [
-				[0, $(series).find('stops zero').text()],
-				[1, $(series).find('stops one').text()]
-			]
-		},
-		data: []
-	};
-	var points = $(series).find('points').text().split(',');
-	$.each(points, function(i, point) {
-		seriesOptions.data.push(parseInt(point));
-	});
-	arel.Plugin.Chart1.options.series.push(seriesOptions);
-});
+		var $xml = $(xml);
+		$xml.find('categories item').each(function(i, category)
+		{
+			options.xAxis.categories.push($(category).text());
+		});
+		$xml.find('series').each(function(i, series)
+		{
+			var seriesOptions =
+			{
+				name: $(series).find('name').text(),
+				color: {
+					linearGradient: { x1: $(series).find('x1').text(), x2: $(series).find('x2').text(), y1: $(series).find('y1').text(), y1: $(series).find('y2').text() },
+					stops: [
+						[0, $(series).find('stops zero').text()],
+						[1, $(series).find('stops one').text()]
+					]
+				},
+				data: []
+			};
+			var points = $(series).find('points').text().split(',');
+			$.each(points, function(i, point) {
+				seriesOptions.data.push(parseInt(point));
+			});
+			options.series.push(seriesOptions);
+		});
+		return $('#' + id).highcharts(options);
+	})
+	.fail(function() { console.log("Failed to load data")})
+	.done(function() { console.log("Loaded data successfully")});
+};
