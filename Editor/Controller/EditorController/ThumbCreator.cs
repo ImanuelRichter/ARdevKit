6f57660cdb045c5a8ca4b8f46084083f758f89c8
cr0ssing <soowerbekomm@gmx.de>
2014-02-15 19:43:11 +0100

    cleanup ThumbCreator

--- Editor/Controller/EditorController/ThumbCreator.cs
+++ Editor/Controller/EditorController/ThumbCreator.cs
@@ -6,16 +6,10 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
-//using MediaBrowser.Library.Logging;
  
 namespace ARdevKit.Controller.EditorController
 {
     class ThumbCreator {
- 
-        static readonly Guid videoType = new
-                  System.Guid("73646976-0000-0010-8000-00AA00389B71");
- 
- 
         public static Bitmap CreateThumb(string videoFilename, double positionPercent)
         {
             Stream ms = new FileStream("tempthump.bmp",FileMode.Create);
