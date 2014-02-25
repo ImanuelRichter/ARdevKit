b327ea8607865f0dd981b305f21cad43d7a7e1d5
Bareus <sed_chan@hotmail.de>
2014-02-25 11:18:51 +0100

    killed height/width for all augmentation and rotationVector for charts from the propertyGrid (not the code!)

--- Editor/Model/Project/Abstract2DAugmentation.cs
+++ Editor/Model/Project/Abstract2DAugmentation.cs
@@ -28,7 +28,7 @@ namespace ARdevKit.Model.Project
         /// <value>
         /// The height, in mm.
         /// </value>
-        [CategoryAttribute("General")]
+        [Browsable(false)]
         [Editor(typeof(SliderEditor), typeof(System.Drawing.Design.UITypeEditor))]
         public int Height
         {
@@ -55,7 +55,7 @@ namespace ARdevKit.Model.Project
         /// <value>
         /// The width, in mm.
         /// </value>
-        [CategoryAttribute("General")]
+        [Browsable(false)]
         [Editor(typeof(SliderEditor), typeof(System.Drawing.Design.UITypeEditor))]
         public int Width
         {
