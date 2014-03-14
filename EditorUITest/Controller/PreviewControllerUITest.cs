ed85c89e37fe116b130ade3be1a5e25bbbd5ad99
Bareus <sed_chan@hotmail.de>
2014-03-14 17:54:17 +0100

    nearly fully implemented TF112**

--- EditorUITest/Controller/PreviewControllerUITest.cs
+++ EditorUITest/Controller/PreviewControllerUITest.cs
@@ -32,15 +32,91 @@ namespace EditorUITest.Controller
         public void copyPaste1()
         {
             // Prepare project
-            MessageBox.Show(new Form() { TopMost = true }, "Füge einen (beliebigen) Trackable dem Projekt hinzu.", "Schritt 1/2");
-            MessageBox.Show(new Form() { TopMost = true }, "Füge eine Chart dem Projekt hinzu.", "Schritt 1/2");
+            copyPasteTemplate("Chart", "TF11210 and TF11220");
 
-            MessageBox.Show(new Form() { TopMost = true }, "Kopiere die Chart und füge sie in die gleiche Scene ein (Bearbeiten -> Kopieren).", "Schritt 1/2");
-            Assert.IsTrue(MessageBox.Show(new Form() { TopMost = true }, "Hat die kopierte Chart die selben Eigenschaten wie das original (Ausnahmen sind die Eigenschaften Option und TranslationVector)?", "Test 4",
+            Assert.IsTrue(MessageBox.Show(new Form() { TopMost = true }, "Hat der Options-Pfad einen fast identischen Pfad wie das original (der letzte Ordner hat die ID als Namen)?", "Test 4",
                 MessageBoxButtons.YesNo) == DialogResult.Yes);
             Assert.IsTrue(MessageBox.Show(new Form() { TopMost = true }, "Hat die ID der Chart sich geändert?", "Test 4",
                 MessageBoxButtons.YesNo) == DialogResult.Yes);
-            Assert.IsTrue(MessageBox.Show(new Form() { TopMost = true }, "Hat der Options-Pfad einen fast identischen Pfad wie das original (der letzte Ordner hat die ID als Namen)?", "Test 4",
+
+            MessageBox.Show(new Form() { TopMost = true }, "Ändere den Options-Pfad der neuen Chart.", "Schritt 1/2");
+            Assert.IsTrue(MessageBox.Show(new Form() { TopMost = true }, "Unterscheidet sich der Options-Pfad der Kopie mit dem Original?", "Test 4",
+                MessageBoxButtons.YesNo) == DialogResult.Yes);
+        }
+
+        [TestMethod]
+        public void copyPaste2()
+        {
+            copyPasteTemplate("ImageAugmentation", "TF11210 and TF11220");
+
+            MessageBox.Show(new Form() { TopMost = true }, "Ändere das Bild der neuen ImageAugmentation.", "Schritt 1/2");
+            Assert.IsTrue(MessageBox.Show(new Form() { TopMost = true }, "Unterscheidet sich das Bild der Kopie mit dem Original?", "Test 4",
+                MessageBoxButtons.YesNo) == DialogResult.Yes);
+        }
+
+        [TestMethod]
+        public void copyPaste3()
+        {
+            copyPasteTemplate("VideoAugmentation", "TF11210 and TF11220");
+
+            MessageBox.Show(new Form() { TopMost = true }, "Ändere das Video der neuen VideoAugmentation.", "Schritt 1/2");
+            Assert.IsTrue(MessageBox.Show(new Form() { TopMost = true }, "Unterscheidet sich das Video der Kopie mit dem Original?", "Test 4",
+                MessageBoxButtons.YesNo) == DialogResult.Yes);
+        }
+
+        [TestMethod]
+        public void copyPast4()
+        {
+            MessageBox.Show(new Form() { TopMost = true }, "Füge einen (beliebigen) Trackable dem Projekt hinzu.", "Schritt 1/2");
+            MessageBox.Show(new Form() { TopMost = true }, "Jeweils eine Augmentation von jedem Typ hinzu.", "Schritt 1/2");
+            MessageBox.Show(new Form() { TopMost = true }, "Erstelle eine zweite Szene", "Schritt 1/2");
+            MessageBox.Show(new Form() { TopMost = true }, "Kopiere jeweils eine Augmentation und füge diese in die zweite Szene ein (mit der Tastenkombination STRG+C und STRG+V)", "Schritt 1/2");
+
+            Assert.IsTrue(MessageBox.Show(new Form() { TopMost = true }, "Haben alle kopierten Augmentationen die selben Eigenschaften wie deren Original (Ausnahmen sind die Eigenschaften Option, TranslationVector und ID)?", "Test 4",
+                MessageBoxButtons.YesNo) == DialogResult.Yes);
+        }
+
+        [TestMethod]
+        public void copyPasteDelete()
+        {
+            MessageBox.Show(new Form() { TopMost = true }, "Füge einen (beliebigen) Trackable dem Projekt hinzu.", "Schritt 1/2");
+            MessageBox.Show(new Form() { TopMost = true }, "Füge einen (beliebigen) Augmentation dem Projekt hinzu.", "Schritt 1/2");
+
+            MessageBox.Show(new Form() { TopMost = true }, "Kopiere und füge die Augmentation in dieselbe Szene ein", "Schritt 1/2");
+            Assert.IsTrue(MessageBox.Show(new Form() { TopMost = true }, "Hat die kopierte Augmentation die selben Eigenschaften wie das Original (Ausnahmen sind die Eigenschaften Option, TranslationVector und ID)?", "Test 4",
+                MessageBoxButtons.YesNo) == DialogResult.Yes);
+
+            MessageBox.Show(new Form() { TopMost = true }, "Erstelle eine zweite Szene mit einer (beliebigen) Trackable und füge da wieder die Augmentation ein", "Schritt 1/2");
+            Assert.IsTrue(MessageBox.Show(new Form() { TopMost = true }, "Hat die kopierte Augmentation die selben Eigenschaften wie das Original (Ausnahmen sind die Eigenschaften Option, TranslationVector und ID)?", "Test 4",
+                MessageBoxButtons.YesNo) == DialogResult.Yes);
+
+            MessageBox.Show(new Form() { TopMost = true }, "Lösche die zuerst erstelle Augmentation in der ersten Szene.", "Schritt 1/2");
+            Assert.IsTrue(MessageBox.Show(new Form() { TopMost = true }, "Ist die gelöschte Augmentation noch auf dem Previewpanel?", "Test 4",
+                MessageBoxButtons.YesNo) == DialogResult.Yes);
+
+            MessageBox.Show(new Form() { TopMost = true }, "Gehe zur zweiten Szene, kopiere diese und lösch danach die Augmentation. Danach gehe zur ersten Szene und füge da die Augmentation ein", "Schritt 1/2");
+            Assert.IsTrue(MessageBox.Show(new Form() { TopMost = true }, "Ist die gelöschte Augmentation noch auf dem Previewpanel in der zweiten Szene?", "Test 4",
+                MessageBoxButtons.YesNo) == DialogResult.Yes);
+            Assert.IsTrue(MessageBox.Show(new Form() { TopMost = true }, "Hat die kopierte Augmentation die selben Eigenschaten wie das original (Ausnahmen sind die Eigenschaften Option, TranslationVector und ID)?", "Schritt 1/2",
+                MessageBoxButtons.YesNo) == DialogResult.Yes);
+        }
+
+        private void copyPasteTemplate(string augmentation, string testName)
+        {
+            // Prepare project
+            MessageBox.Show(new Form() { TopMost = true }, "Füge einen (beliebigen) Trackable dem Projekt hinzu.", testName);
+            MessageBox.Show(new Form() { TopMost = true }, "Füge eine " + augmentation + " dem Projekt hinzu.", testName);
+
+            Assert.IsTrue(MessageBox.Show(new Form() { TopMost = true }, "Ist der Button in Bearbeiten -> Einfügen disabled?", testName,
+                MessageBoxButtons.YesNo) == DialogResult.Yes);
+
+            MessageBox.Show(new Form() { TopMost = true }, "Kopiere die " + augmentation + " (Bearbeiten -> Kopieren).", testName);
+
+            Assert.IsTrue(MessageBox.Show(new Form() { TopMost = true }, "Ist der Button in Bearbeiten -> Einfügen enabled?", testName,
+                MessageBoxButtons.YesNo) == DialogResult.Yes);
+
+            MessageBox.Show(new Form() { TopMost = true }, "Füge die " + augmentation + " in die selbe Szene ein.", testName);
+            Assert.IsTrue(MessageBox.Show(new Form() { TopMost = true }, "Hat die kopierte " + augmentation + " die selben Eigenschaten wie das original (Ausnahmen sind die Eigenschaften Option, TranslationVector und ID)?", testName,
                 MessageBoxButtons.YesNo) == DialogResult.Yes);
         }
 
