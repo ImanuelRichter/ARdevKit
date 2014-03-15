18a969ea73bde3dd346192a1c5a84a275544f664
Bareus <sed_chan@hotmail.de>
2014-03-15 02:32:46 +0100

    ups forgot this.. read previous commit

---
+++ EditorTest/EditorWindowFormTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ARdevKit;
+
+namespace EditorTest
+{
+    [TestClass]
+    public class EditorWindowFormTest
+    {
+        private EditorWindow ew;
+
+        [TestInitialize]
+        public void initTest()
+        {
+            ew = new EditorWindow();
+        }
+
+        [TestMethod]
+        public void testCreateNewProject1()
+        {
+            ew.createNewProject(null);
+            Assert.IsTrue(ew.project.Name == null);
+        }
+
+        [TestMethod]
+        public void testCreateNewProject2()
+        {
+            ew.createNewProject("");
+            Assert.IsTrue(String.Compare(ew.project.Name , "") == 0);
+        }
+    }
+}
